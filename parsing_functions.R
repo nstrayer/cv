06eef0b9a97f4364fc56840e677c678d1942e44d
Nick Strayer <nick.strayer@gmail.com>
2019-09-30 11:53:24 -0500

    Changed css to use overriding instead of just modifying the entirety of the original resume css. Also offloaded the parsing files to their own script that gets sourced.

---
+++ parsing_functions.R
@@ -0,0 +1,84 @@
+# Regex to locate links in text
+find_link <- regex("
+  \\[   # Grab opening square bracket
+  .+?   # Find smallest internal text as possible
+  \\]   # Closing square bracket
+  \\(   # Opening parenthesis
+  .+?   # Link text, again as small as possible
+  \\)   # Closing parenthesis
+  ",
+                   comments = TRUE)
+
+# Function that removes links from text and replaces them with superscripts that are 
+# referenced in an end-of-document list. 
+sanitize_links <- function(text){
+  if(PDF_EXPORT){
+    str_extract_all(text, find_link) %>% 
+      pluck(1) %>% 
+      walk(function(link_from_text){
+        title <- link_from_text %>% str_extract('\\[.+\\]') %>% str_remove_all('\\[|\\]') 
+        link <- link_from_text %>% str_extract('\\(.+\\)') %>% str_remove_all('\\(|\\)')
+        
+        # add link to links array
+        links <<- c(links, link)
+        
+        # Build replacement text
+        new_text <- glue('{title}<sup>{length(links)}</sup>')
+        
+        # Replace text
+        text <<- text %>% str_replace(fixed(link_from_text), new_text)
+      })
+  }
+  
+  text
+}
+
+
+# Takes a single row of dataframe corresponding to a position
+# turns it into markdown, and prints the result to console.
+build_position_from_df <- function(pos_df){
+  
+  missing_start <- pos_df$start == 'N/A'
+  dates_same <- pos_df$end == pos_df$start
+  
+  if(any(c(missing_start,dates_same))){
+    timeline <- pos_df$end
+  } else {
+    timeline <- glue('{pos_df$end} - {pos_df$start}')
+  }
+  
+  descriptions <- pos_df[str_detect(names(pos_df), 'description')] %>% 
+    as.list() %>% 
+    map_chr(sanitize_links)
+  
+  # Make sure we only keep filled in descriptions
+  description_bullets <- paste('-', descriptions[descriptions != 'N/A'], collapse = '\n')
+  
+  glue(
+    "### {sanitize_links(pos_df$title)}
+
+{pos_df$loc}
+
+{pos_df$institution}
+
+{timeline}
+
+{description_bullets}
+
+
+"
+  ) %>% print()
+}
+
+# Takes nested position data and a given section id 
+# and prints all the positions in that section to console
+print_section <- function(position_data, section_id){
+  position_data %>% 
+    filter(section == section_id) %>% 
+    pull(data) %>% 
+    purrr::walk(build_position_from_df)
+}
+
+fill_nas <- function(column){
+  ifelse(is.na(column), 'N/A', column)
+}
