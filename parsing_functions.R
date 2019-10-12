4923ba4dbd7d859d8dd521b51a2997baac0db766
Nick Strayer <nick.strayer@gmail.com>
2019-10-12 09:16:23 -0500

    Changed skills bars to use css linear gradient to encode values allowing for them to have a background.

--- parsing_functions.R
+++ parsing_functions.R
@@ -89,11 +89,15 @@ print_section <- function(position_data, section_id){
 
 # Construct a bar chart of skills
 build_skill_bars <- function(skills, out_of = 5){
+  bar_color <- "#969696"
+  bar_background <- "#d9d9d9"
   skills %>% 
-    arrange(desc(level)) %>% 
+    mutate(width_percent = round(100*level/out_of)) %>% 
     glue_data(
       "<div class = 'skill-bar'",
-      "style='width:{round(100*level/out_of)}%;'>",
+      "style = \"background:linear-gradient(to right,",
+      "{bar_color} {width_percent}%,",
+      "{bar_background} {width_percent}% 100%)\" >",
       "{skill}",
       "</div>"
     )
