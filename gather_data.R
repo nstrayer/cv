13bf8cf34fce19f3c8a050f3ba740ab74fa901bc
keita43a <keita43a@gmail.com>
2020-02-19 18:05:26 +0100

    New commit for every file

--- gather_data.R
+++ gather_data.R
@@ -20,12 +20,8 @@ if(using_googlesheets){
   
   
   position_data <- read_sheet(positions_sheet_loc, sheet = "positions")
-<<<<<<< HEAD
   skills        <- read_sheet(positions_sheet_loc, sheet = "skills")
   lang        <- read_sheet(positions_sheet_loc, sheet = "language")
-=======
-  skills        <- read_sheet(positions_sheet_loc, sheet = "language_skills")
->>>>>>> origin/master
   text_blocks   <- read_sheet(positions_sheet_loc, sheet = "text_blocks")
   contact_info  <- read_sheet(positions_sheet_loc, sheet = "contact_info", skip = 1)
   
