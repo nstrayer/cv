59c67e8492010ff8a29776fba0d640c95ead6815
Nick Strayer <nick.strayer@gmail.com>
2020-05-08 11:50:24 -0500

    Cleaned up text processing and added better data loading logic

--- CV_Printer.R
+++ CV_Printer.R
@@ -3,18 +3,6 @@ library(tidyverse)
 library(glue)
 library(googlesheets4)
 
-find_link <- regex(
-  "
-  \\[   # Grab opening square bracket
-  .+?   # Find smallest internal text as possible
-  \\]   # Closing square bracket
-  \\(   # Opening parenthesis
-  .+?   # Link text, again as small as possible
-  \\)   # Closing parenthesis
-  ",
-  comments = TRUE
-)
-
 link_header <- "
 Links {data-icon=link}
 --------------------------------------------------------------------------------
@@ -31,6 +19,20 @@ pdf_style <- "
 }
 </style>"
 
+
+default_position_entry_template <- 
+"### {title}
+
+{loc}
+
+{institution}
+
+{timeline}
+
+{description_bullets}
+\n\n\n"
+
+
 # Tests if the end date is set as current, via values in the current_names vector
 date_is_current <- function(date){
   current_names <- c("current", "now", "")
@@ -45,46 +47,69 @@ CV_Printer <- R6::R6Class("CV_Printer", list(
   skills        = dplyr::tibble(),
   text_blocks   = dplyr::tibble(),
   contact_info  = dplyr::tibble(),
+  position_entry_template = default_position_entry_template,
   pdf_mode = FALSE,
   html_location = "",
   pdf_location = "",
   links = c(), 
-  initialize = function(data_loc, pdf = FALSE, html_location, pdf_location) {
+  initialize = function(data_loc, 
+                        pdf = FALSE, 
+                        html_location, 
+                        pdf_location, 
+                        position_entry_template = default_position_entry_template,
+                        sheet_is_publicly_readable = TRUE) {
     self$pdf_mode <- pdf
     self$html_location <- html_location
     self$pdf_location <- pdf_location
+    self$position_entry_template = position_entry_template
     
-    is_google_sheets_loc <- TRUE
+    is_google_sheets_loc <- stringr::str_detect(data_loc, "docs\\.google\\.com")
     if(is_google_sheets_loc){
-      options(gargle_oauth_cache = ".secrets")
+      
+      if(sheet_is_publicly_readable){
+        # This tells google sheets to not try and authenticate. Note that this will only
+        # work if your sheet has sharing set to "anyone with link can view"
+        sheets_deauth()
+      } else {
+        # My info is in a public sheet so there's no need to do authentication but if you want
+        # to use a private sheet, then this is the way you need to do it.
+        # designate project-specific cache so we can render Rmd without problems
+        options(gargle_oauth_cache = ".secrets")
+      }
       
       self$position_data <- read_sheet(data_loc, sheet = "positions")
       self$skills        <- read_sheet(data_loc, sheet = "language_skills")
       self$text_blocks   <- read_sheet(data_loc, sheet = "text_blocks")
       self$contact_info  <- read_sheet(data_loc, sheet = "contact_info", skip = 1)
     } else {
-      # ToDo Read in from csv location
+      # Want to go oldschool with just a csv?
+      self$position_data <- read_csv(paste0(data_loc, "positions.csv"))
+      self$skills        <- read_csv(paste0(data_loc, "language_skills.csv"))
+      self$text_blocks   <- read_csv(paste0(data_loc, "text_blocks.csv"))
+      self$contact_info  <- read_csv(paste0(data_loc, "contact_info.csv"), skip = 1)
     }
+    
+  },
+  set_pdf_mode = function(pdf_mode = TRUE){
+    self$pdf_mode <- pdf_mode
+    invisible(self)
   },
   sanitize_links = function(text){
     out_text <- text
     if(self$pdf_mode){
-      str_extract_all(out_text, find_link) %>% 
-        pluck(1) %>% 
-        walk(function(link_from_text){
-          title <- link_from_text %>% str_extract('\\[.+\\]') %>% str_remove_all('\\[|\\]') 
-          link <- link_from_text %>% str_extract('\\(.+\\)') %>% str_remove_all('\\(|\\)')
-          
-          # add link to links array
-          self$links <- c(self$links, link)
-          
-          # Build replacement text
-          new_text <- glue('{title}<sup>{length(self$links)}</sup>')
-          
-          # Replace text
-          out_text <<- out_text %>% str_replace(fixed(link_from_text), new_text)
-        })
-    }
+      link_titles <- str_extract_all(text, '(?<=\\[).+?(?=\\])')[[1]]
+      link_destinations <- str_extract_all(text, '(?<=\\().+?(?=\\))')[[1]]
+      n_links <- length(link_titles)
+      if(n_links > 0){
+        # add links to links array
+        self$links <- c(self$links, link_destinations)
+        
+        out_text <- text %>% 
+          str_replace_all(set_names(paste0("<sup>", 1:n_links, "</sup>"), 
+                                    paste0("\\(", link_destinations, "\\)"))) %>% 
+          str_replace_all(set_names(link_titles, paste0("\\[", link_titles, "\\]")))
+      }
+    } 
     out_text
   },
   # Take entire positions dataframe and removes the links 
@@ -138,26 +163,16 @@ CV_Printer <- R6::R6Class("CV_Printer", list(
       ) %>% 
       self$strip_links_from_cols(c('title', 'description_bullets')) %>% 
       mutate_all(~ifelse(is.na(.), 'N/A', .)) %>% 
-      glue_data(
-        "### {title}",
-        "\n\n",
-        "{loc}",
-        "\n\n",
-        "{institution}",
-        "\n\n",
-        "{timeline}", 
-        "\n\n",
-        "{description_bullets}",
-        "\n\n\n",
-      )
+      glue_data(self$position_entry_template)
   },
   print_text_block = function(label){
-    filter(self$text_blocks, loc == label)$text %>%
+    filter(self$text_blocks, loc == label) %>% 
+      pull(text) %>%
       self$sanitize_links() %>%
       cat()
   },
   # Construct a bar chart of skills
-  build_skill_bars = function(out_of = 5){
+  print_skill_bars = function(out_of = 5){
     bar_color <- "#969696"
     bar_background <- "#d9d9d9"
     self$skills %>% 
@@ -199,8 +214,7 @@ CV_Printer <- R6::R6Class("CV_Printer", list(
       cat(pdf_style)
     }
   }
-)
-)
+))
 
 
 
@@ -215,12 +229,15 @@ CV_Printer <- R6::R6Class("CV_Printer", list(
 # 
 # nicks_cv$print_section("teaching_positions")
 
-nicks_cv_pdf <- CV_Printer$new(
-  data_loc = "https://docs.google.com/spreadsheets/d/14MQICF2F8-vf8CKPF1m4lyGKO6_thG-4aSwat1e2TWc",  
-  pdf = TRUE,
-  pdf_location = "https://github.com/nstrayer/cv/raw/master/strayer_cv.pdf",
-  html_location = "nickstrayer.me/cv/")
-
-nicks_cv_pdf$print_section("teaching_positions")
-nicks_cv_pdf$print_text_block("intro")
-nicks_cv_pdf$print_links()
+# Need to run this once before knitting to cache an authentication token
+# # googlesheets4::sheets_auth()
+# 
+# nicks_cv_pdf <- CV_Printer$new(
+#   data_loc = "https://docs.google.com/spreadsheets/d/14MQICF2F8-vf8CKPF1m4lyGKO6_thG-4aSwat1e2TWc",
+#   pdf = TRUE,
+#   pdf_location = "https://github.com/nstrayer/cv/raw/master/strayer_cv.pdf",
+#   html_location = "nickstrayer.me/cv/")
+# 
+# nicks_cv_pdf$print_section("teaching_positions")
+# nicks_cv_pdf$print_text_block("intro")
+# nicks_cv_pdf$print_links()
